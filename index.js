08c76286e0cd435de9b0c1a59055c6d21cba57d1
Ran Greenberg <rang@wix.com>
2018-11-03 19:16:32 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import {Navigation} from "react-native-navigation";
+import {AppRegistry} from 'react-native';
+import App from "./App";
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+      }
+    }
+  });
+});
