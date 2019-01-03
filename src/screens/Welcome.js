f81cbe4558d492213f52035f1def5b9ccd64aecc
Ran Greenberg <rang@wix.com>
2019-01-03 18:29:27 +0200

    update ui

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,31 +1,49 @@
 import React, {Component} from 'react';
 import {Platform} from 'react-native';
 import {Text, View, Button} from 'react-native-ui-lib';
+import {Navigation} from 'react-native-navigation';
 import * as actions from './../stores/actions';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-  'Double tap R on your keyboard to reload,\n' +
-  'Shake or press menu button for dev menu',
-});
-const details = 'Using:\n' +
-  'react-native-navigation\n' +
-  'react-native-ui-lib\n';
+const details = 'react-native-navigation\n' +
+  'react-native-ui-lib\n' +
+  'remx\n';
 
 type Props = {};
 export default class Welcome extends Component<Props> {
+
+  static options() {
+    return {
+      topBar: {
+        visible: false
+      }
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    Navigation.mergeOptions(this.props.componentId, {
+      topBar: {
+        visible: false
+      }
+    });
+  }
 
   onShowMorePress = () => {
     actions.pushShowMeMoreScreen(this.props.componentId);
   };
 
+  renderText(text) {
+    return (
+      <Text center white text40 marginB-40>{`${text}`}</Text>
+    );
+
+  }
+
   render() {
     return (
       <View flex center bg-dark20>
-        <Text center white text40 marginB-10>{'Welcome to React Native\n💀 Skeleton 💀'}</Text>
-        <Text center white text70 marginB-10>To get started, edit App.js</Text>
-        <Text center white text70 marginB-15>{instructions}</Text>
+        {this.renderText('Welcome to React Native\n💀 Skeleton 💀')}
+        {this.renderText('To get started, edit App.js')}
         <Text center white text60>{details}</Text>
         <Button
           onPress={this.onShowMorePress}
